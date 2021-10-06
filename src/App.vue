f9909a41541803335bb9323a9684ed0942ec3837
tcy <1131857210@Qq.com>
2021-10-06 22:57:08 +0800

    money  样式

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,15 @@
 </template>
 
 <style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 body{
   line-height: 1.5;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:$font-hei;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-a{
-  text-decoration: none;
-  color: inherit;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #333;
+  font-size: 16px;
 }
 </style>
 <script>
